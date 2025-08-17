6acaba27a747111ab06cf720c5f695f63e562504
unknown <mohammed98ali.ma@gmail.com>
2025-08-17 09:11:40 +0300

    pokemon router

---
+++ routes/pokemonRoute/pokemonRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const pokemonController = require('../controllers/pokemons')
+
+router.post('/new', pokemonController.createPokemon)
+router.get('/', pokemonController.pokemonIndex)
+router.get('/:id', pokemonController.showPokemon)
+router.put('/:id', pokemonController.updatePokemon)
+router.delete('/:id', pokemonController.deletePokemon)
+module.exports = router
