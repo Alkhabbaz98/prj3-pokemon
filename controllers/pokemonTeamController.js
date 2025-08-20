4a599fb9a75142c16033e667b01295116ad5d772
Your Name <you@example.com>
2025-08-20 20:04:29 +0300

    populate user

--- controllers/pokemonTeamController.js
+++ controllers/pokemonTeamController.js
@@ -1,78 +1,70 @@
-const PokeTeam = require('../models/Pokemon/PokeTeam')
-const jwt = require('jsonwebtoken')
-async function createPokeTeam(req, res){
-// const user = jwt.decode(req.headers.authorization)
+const PokeTeam = require("../models/Pokemon/PokeTeam");
+const jwt = require("jsonwebtoken");
+async function createPokeTeam(req, res) {
+  // const user = jwt.decode(req.headers.authorization)
 
-    console.log('You are in the create Pokemon controller')
-    try {
-        console.log('Req.body is: ', req.body)
-        const createdPokeTeam = await PokeTeam.create({
-            ...req.body,
-            user: req.user.id
-        }
-            
-        )
-        res.status(201).json(createdPokeTeam)
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({error: err.message})
-    }
+  console.log("You are in the create Pokemon controller");
+  try {
+    console.log("Req.body is: ", req.body);
+    const createdPokeTeam = await PokeTeam.create({
+      ...req.body,
+      user: req.user.id,
+    });
+    res.status(201).json(createdPokeTeam);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
 }
 
-async function pokeTeamIndex(req, res){
-    try {
-        const allPokeTeams = await PokeTeam.find()
-        if (allPokeTeams.length){
-            res.status(200).json(allPokeTeams)
-        } else {
-            res.sendStatus(204)
-        }
-        
-    } catch (err){
-        console.log(err)
+async function pokeTeamIndex(req, res) {
+  try {
+    const allPokeTeams = await PokeTeam.find().populate("user", "username");
+    if (allPokeTeams.length) {
+      res.status(200).json(allPokeTeams);
+    } else {
+      res.sendStatus(204);
     }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function showPokeTeam(req, res){
-    try {
-        const team = await PokeTeam.findById(req.params.id)
-        if (team){
-            res.status(200).json(team)
-        } else {
-            res.sendStatus(404)
-        }
-        
-    } catch (err) {
-        res.status(500).json({error: err.message})
+async function showPokeTeam(req, res) {
+  try {
+    const team = await PokeTeam.findById(req.params.id);
+    if (team) {
+      res.status(200).json(team);
+    } else {
+      res.sendStatus(404);
     }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
 
-async function deletePokeTeam(req, res){
-    try {
-        const team = await PokeTeam.findByIdAndDelete(req.params.id)
-        if (team){
-            res.status(200).json(team)
-        } else {
-            res.sendStatus(404)
-        }
-        
-
-    } catch (err){
-        res.status(500).json({error: err.message})
+async function deletePokeTeam(req, res) {
+  try {
+    const team = await PokeTeam.findByIdAndDelete(req.params.id);
+    if (team) {
+      res.status(200).json(team);
+    } else {
+      res.sendStatus(404);
     }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
 
-async function updatePokeTeam(req, res){
-    try {
-        const team = await PokeTeam.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            {new: true}
-        )
-        res.status(200).json(team)
-    } catch (err){
-        res.status(500).json({error: err.message})
-    }
+async function updatePokeTeam(req, res) {
+  try {
+    const team = await PokeTeam.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(team);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
 
 // ======================================
@@ -93,8 +85,6 @@ async function updatePokeTeam(req, res){
 //     }
 // }
 
-
-
 // async function deleteMoveset(req, res){
 //     try {
 //         const pokemon = await Pokemon.findById(req.params.id)
@@ -108,7 +98,7 @@ async function updatePokeTeam(req, res){
 // }
 
 // async function updateMoveset(req, res){
-   
+
 //     try {
 //         const pokemon = await Pokemon.findById(req.params.id)
 //         console.log('Req.body is: ', req.body)
@@ -129,7 +119,6 @@ async function updatePokeTeam(req, res){
 //     }
 // }
 
-
 // // ======================================
 
 // async function createStats(req, res){
@@ -147,8 +136,6 @@ async function updatePokeTeam(req, res){
 //     }
 // }
 
-
-
 // async function deleteStats(req, res){
 //     try {
 //         const pokemon = await Pokemon.findById(req.params.id)
@@ -162,7 +149,7 @@ async function updatePokeTeam(req, res){
 // }
 
 // async function updateStats(req, res){
-   
+
 //     try {
 //         const pokemon = await Pokemon.findById(req.params.id)
 //         console.log('Req.body is: ', req.body)
@@ -182,16 +169,10 @@ async function updatePokeTeam(req, res){
 //     }
 // }
 
-
-
-
-
-    
-    
-    module.exports = {
-    createPokeTeam,    
-    pokeTeamIndex,
-    showPokeTeam,
-    deletePokeTeam,
-    updatePokeTeam,
-}
+module.exports = {
+  createPokeTeam,
+  pokeTeamIndex,
+  showPokeTeam,
+  deletePokeTeam,
+  updatePokeTeam,
+};
